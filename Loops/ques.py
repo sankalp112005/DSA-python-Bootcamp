2c2a6b10797230ff7c9a2bd52a77e2111045d894
Sankalp Jain <sankalpj649@gmail.com>
2026-02-26 20:22:20 +0530

    adding files of loops

---
+++ Loops/ques.py
@@ -0,0 +1,11 @@
+#1. PRINTING N TIME THE SENTENCE
+'''n=int(input('Enter the time you want to print the sentence'))
+for i in range(n):
+    print("hii sankalp!!")'''
+
+#2. PRINT NTURAL NUMBERS UPTO N
+'''a=int(input('tell your number upto you want'))
+for i in range(1,a+1):
+    print (i)'''
+
+#3.
