73c895ef52c56a7e36fc9cea9fb3ce635d8f3c2a
Sankalp Jain <sankalpj649@gmail.com>
2026-02-28 00:00:51 +0530

    Fix input prompts and print statement formatting

--- Loops/ques.py
+++ Loops/ques.py
@@ -1,11 +1,11 @@
 #1. PRINTING N TIME THE SENTENCE
-'''n=int(input('Enter the time you want to print the sentence'))
+n=int(input('Enter the time you want to print the sentence'))
 for i in range(n):
-    print("hii sankalp!!")'''
+    print("hii sankalp!!")
 
 #2. PRINT NTURAL NUMBERS UPTO N
-'''a=int(input('tell your number upto you want'))
+a=int(input('tell your number upto you want'))
 for i in range(1,a+1):
-    print (i)'''
+    print (i)
 
 #3.
